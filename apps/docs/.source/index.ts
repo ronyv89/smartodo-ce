1211584454f2e7dd33fff6156865fb939bbc94b3
Rony Varghese <ronyv250289@gmail.com>
2026-04-18 08:19:37 +0530

    feature: setup fumadocs as application document app

---
+++ apps/docs/.source/index.ts
@@ -0,0 +1,5 @@
+// @ts-nocheck -- skip type checking
+import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1776311203743"
+import { _runtime } from "fumadocs-mdx"
+import * as _source from "../source.config"
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"C:/Users/Rony/Develop/Projects/smartodo-ce/apps/docs/content/docs/index.mdx"}, data: docs_0 }], [{"info":{"path":"meta.json","absolutePath":"C:/Users/Rony/Develop/Projects/smartodo-ce/apps/docs/content/docs/meta.json"},"data":{"title":"Documentation","pages":["index"]}}])
